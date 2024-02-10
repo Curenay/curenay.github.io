6ca8b093dede34da6f24fbbaae236df3cf331505
Curenay <curenay@gmail.com>
2024-02-11 10:59:24 +1100

    Visit

--- au/js/templatemo-script.js
+++ au/js/templatemo-script.js
@@ -87,3 +87,11 @@ jQuery(function() {
       closeMenu();
     });
 });
+
+//Visit
+  window.smartlook||(function(d) {
+    var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+    var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+    c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
+    })(document);
+    smartlook('init', 'ea2f7cbbe654ae11e8ec8bb86fc49a59d96f596e', { region: 'eu' });
